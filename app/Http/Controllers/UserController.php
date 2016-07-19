e3af5a812e51b77ae5515b1852e701950676fcc0
ryzhykE <stoyatboy@gmail.com>
2016-07-19 22:51:39 +0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Input;
 class UserController extends Controller
 { 
    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     
     public function index(){
         
