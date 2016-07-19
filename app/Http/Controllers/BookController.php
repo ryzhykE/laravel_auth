69f899e7a2180f3a44d5d3283ed22975e2ad93dc
ryzhykE <stoyatboy@gmail.com>
2016-07-19 22:51:06 +0300

    Update BookController.php

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Input;
 
 class BookController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     
     public function index() {
         
